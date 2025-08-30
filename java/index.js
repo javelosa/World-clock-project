15ee970b4d78862d7580b981e2ea1fc2361af15d
javelosa <49219594+javelosa@users.noreply.github.com>
2025-08-30 15:20:34 +0800

    Added current location timezone

--- java/index.js
+++ java/index.js
@@ -50,6 +50,9 @@ if (losAngelesElement) {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace(/_/g, " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
